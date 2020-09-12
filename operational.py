3ee952fe35b3df5dc011d9857ab7972af34580ad
Wind-Wing <windwing.me@gmail.com>
2020-09-13 00:00:53 +0800

    define operational interface

---
+++ operational.py
@@ -0,0 +1,15 @@
+from abc import abstractmethod
+
+
+class Operational:
+    @abstractmethod
+    def operate(self, res, cmd):
+
+    @abstractmethod
+    def get_cmd_length(self):
+        pass
+
+    @abstractmethod
+    def _get_operation_list(self):
+        pass
+
