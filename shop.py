ba20515de8300792c56b2cd98555c0d59cea6d2b
Wind-Wing <windwing.me@gmail.com>
2020-09-13 00:01:21 +0800

    Refactor shop and imp operational interface

--- shop.py
+++ shop.py
@@ -1,50 +1,42 @@
 from common import *
+from operational import Operational
 
 
-class Shop:
+class Shop(Operational):
+    def operate(self, res, cmd):
+        operation_list = self._get_operation_list()
+        assert(len(cmd) == len(operation_list))
+        for i in range(len(cmd)):
+            if cmd[i] == 1:
+                res = operation_list[i](res)
+
+    def get_cmd_length(self):
+        return len(self._get_operation_list())
+
+    def _get_operation_list(self):
+        op_list = list()
+        op_list.append(self.buy_wish_coin)
+        op_list.append(self.buy_draw_voucher)
+        op_list.append(self.buy_consecrate_time)
+        return op_list
+
     @staticmethod
-    def buy_one_day(res):
-        res = Shop.fighting_shop(res)
-        res = Shop.museum_shop(res)
-        res = Shop.underground_shop(res)
-        res = Shop.daily_shop(res)
-        return res
-
-    @staticmethod
-    def daily_shop(res):
+    def buy_wish_coin(res):
         if Common.is_enough(res.whiteTadpole, 600):
             res.whiteTadpole -= 600
             res.wishCoin += 1
+        return res
 
+    @staticmethod
+    def buy_draw_voucher(res):
         if Common.is_enough(res.whiteTadpole, 1500):
             res.whiteTadpole -= 1500
             res.drawVoucher += 8
+        return res
 
+    @staticmethod
+    def buy_consecrate_time(res):
         if Common.is_enough(res.whiteTadpole, 600):
             res.whiteTadpole -= 600
             res.consecrateTime += time(0, 10, 0)
         return res
-
-    @staticmethod
-    def fighting_shop(res):
-        res.drawVoucher += 1. / 3.
-        return res
-
-    @staticmethod
-    def museum_shop(res):
-        res.wishCoin += 1.
-        return res
-
-    @staticmethod
-    def underground_shop(res):
-        if Common.is_enough(res.whiteTadpole, 160):
-            res.whiteTadpole -= 160
-            res.undergroundPoint += 100
-        if Common.is_enough(res.undergroundPoint, 100, "undergroundPoint"):
-            res.undergroundPoint -= 100
-            res.wishCoin += 1. / 3.
-        if Common.is_enough(res.undergroundPoint, 300, "undergroundPoint"):
-            res.undergroundPoint -= 300
-            res.drawVoucher += 2
-        return res
-
